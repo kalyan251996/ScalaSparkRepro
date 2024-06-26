7a3216805b1b4670f468c5bc82ebd24da9c0645c
Kalyan Thalluru <tlskalyan1996@gmail.com>
2024-06-26 08:47:33 +0530

    Count Program

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -4,7 +4,7 @@ object first {
   def main(args:Array[String]):Unit={
 
     val sc=new SparkContext("local[*]","kalyan")
-    val rdd1=sc.textFile("D:/data/kalyan_26.txt")
+    val rdd1=sc.textFile("D:/data/kalyan.txt")
     val rdd2=rdd1.flatMap(x=>x.split(" "))
     val rdd3=rdd2.map(x=>(x,1))
     val rdd4=rdd3.reduceByKey((x,y)=>x+y)
